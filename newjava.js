a300e85d08300f7d1f5884cfb70058c03b136d54
opeyemi <operaomilude111@gmail.com>
2022-11-07 18:02:52 +0100

    about

---
+++ newjava.js
@@ -0,0 +1,5 @@
+document.getElementById('self').innerHTML="About Myself";
+document.getElementById('intro').innerHTML="My name is omilude opeyemi oluwakemi beauty";
+document.getElementById('about').innerHTML="my dream university is berun i want to become a very God fearing, wealthy and successful lawyer one day and i pray it soon";
+document.getElementById().innerHTML="";
+document.getElementById().innerHTML="";
